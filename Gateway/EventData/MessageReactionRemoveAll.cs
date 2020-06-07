431e1bd6a83bbd0bc8db93a429410864706bc301
Matthew Manley <MatthewManley@users.noreply.github.com>
2020-06-07 02:13:11 -0400

    added message reaction remove all event

---
+++ Gateway/EventData/MessageReactionRemoveAll.cs
@@ -0,0 +1,29 @@
+﻿using Gracie.ETF;
+
+namespace Gracie.Gateway.EventData
+{
+    /// <summary>
+    /// Sent when a user explicitly removes all reactions from a message
+    /// https://discord.com/developers/docs/topics/gateway#message-reaction-remove-all
+    /// </summary>
+    public class MessageReactionRemoveAll
+    {
+        /// <summary>
+        /// the id of the channel
+        /// </summary>
+        [EtfProperty("channel_id")]
+        public ulong ChannelId { get; set; }
+
+        /// <summary>
+        /// the id of the message
+        /// </summary>
+        [EtfProperty("message_id")]
+        public ulong MessageId { get; set; }
+
+        /// <summary>
+        /// the id of the guild
+        /// </summary>
+        [EtfProperty("guild_id")]
+        public ulong? GuildId { get; set; }
+    }
+}
--- Gateway/GatewayClient.cs
+++ Gateway/GatewayClient.cs
@@ -103,20 +103,21 @@ namespace Gracie.Gateway
         {
             return new Dictionary<string, DispatchEventHandler>
             {
-                { "READY",                      BuildEvent(() => ReadyReceived) },
-                { "TYPING_START",               BuildEvent(() => TypingStartReceived) },
-                { "GUILD_CREATE",               BuildEvent(() => GuildCreateReceived) },
-                { "MESSAGE_CREATE",             BuildEvent(() => MessageCreateReceived) },
-                { "VOICE_SERVER_UPDATE",        BuildEvent(() => VoiceServerUpdateReceived) },
-                { "VOICE_STATE_UPDATE",         BuildEvent(() => VoiceStateUpdateReceived) },
-                { "USER_UPDATE",                BuildEvent(() => UserUpdateReceived) },
-                { "WEBHOOKS_UPDATE",            BuildEvent(() => WebhooksUpdateReceived) },
-                { "PRESENCE_UPDATE",            BuildEvent(() => PresenceUpdateReceived) },
-                { "MESSAGE_DELETE",             BuildEvent(() => MessageDeleteReceived) },
-                { "MESSAGE_DELETE_BULK",        BuildEvent(() => MessageDeleteBulkReceived) },
-                { "MESSAGE_UPDATE",             BuildEvent(() => MessageUpdateReceived) },
-                { "MESSAGE_REACTION_ADD",       BuildEvent(() => MessageReactionAddReceived) },
-                { "MESSAGE_REACTION_REMOVE",    BuildEvent(() => MessageReactionRemoveReceived) }
+                { "READY",                          BuildEvent(() => ReadyReceived) },
+                { "TYPING_START",                   BuildEvent(() => TypingStartReceived) },
+                { "GUILD_CREATE",                   BuildEvent(() => GuildCreateReceived) },
+                { "MESSAGE_CREATE",                 BuildEvent(() => MessageCreateReceived) },
+                { "VOICE_SERVER_UPDATE",            BuildEvent(() => VoiceServerUpdateReceived) },
+                { "VOICE_STATE_UPDATE",             BuildEvent(() => VoiceStateUpdateReceived) },
+                { "USER_UPDATE",                    BuildEvent(() => UserUpdateReceived) },
+                { "WEBHOOKS_UPDATE",                BuildEvent(() => WebhooksUpdateReceived) },
+                { "PRESENCE_UPDATE",                BuildEvent(() => PresenceUpdateReceived) },
+                { "MESSAGE_DELETE",                 BuildEvent(() => MessageDeleteReceived) },
+                { "MESSAGE_DELETE_BULK",            BuildEvent(() => MessageDeleteBulkReceived) },
+                { "MESSAGE_UPDATE",                 BuildEvent(() => MessageUpdateReceived) },
+                { "MESSAGE_REACTION_ADD",           BuildEvent(() => MessageReactionAddReceived) },
+                { "MESSAGE_REACTION_REMOVE",        BuildEvent(() => MessageReactionRemoveReceived) },
+                { "MESSAGE_REACTION_REMOVE_ALL",    BuildEvent(() => MessageReactionRemoveReceived) }
             };
         }
 
@@ -141,5 +142,6 @@ namespace Gracie.Gateway
         public event GatewayEventHandler<GatewayDataPayload<Message>> MessageUpdateReceived;
         public event GatewayEventHandler<GatewayDataPayload<MessageReactionAdd>> MessageReactionAddReceived;
         public event GatewayEventHandler<GatewayDataPayload<MessageReactionRemove>> MessageReactionRemoveReceived;
+        public event GatewayEventHandler<GatewayDataPayload<MessageReactionRemoveAll>> MessageReactionRemoveAllreceived;
     }
 }
